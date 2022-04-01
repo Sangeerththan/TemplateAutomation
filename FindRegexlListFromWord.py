84fa1bfd393e4a6095db809a8806d6fe937dc5f5
Sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2022-04-01 14:12:24 +0530

    [FEATURE][PENDING] - Extract capital Letters from word

--- FindRegexlListFromWord.py
+++ FindRegexlListFromWord.py
@@ -1,7 +1,25 @@
 import docx2txt
 import re
+import openpyxl
+import pandas as pd
 
-# read in word file
-result = docx2txt.process("BARA158b.docx")
-capital_letters = re.findall(r"\b[A-Z][A-Z]+\b", result)
-print(set(capital_letters))
+
+def getCapitalWordsFromDoc(wordDocument):
+    result = docx2txt.process(wordDocument)
+    capital_letters = re.findall(r"\b[A-Z][A-Z]+\b", result)
+    return set(capital_letters)
+
+
+words = getCapitalWordsFromDoc("BARA158b.docx")
+
+for word in words:
+    print(word)
+
+def readExcel(document, columnIndex):
+    read = pd.read_excel(document)
+    labels = read.iloc[:, columnIndex].tolist()
+    return set(labels)
+
+
+print(set.intersection(readExcel("axxia.xlsx", 8),getCapitalWordsFromDoc("BARA158b.docx")))
+print(getCapitalWordsFromDoc("CRES142a.docx"))
